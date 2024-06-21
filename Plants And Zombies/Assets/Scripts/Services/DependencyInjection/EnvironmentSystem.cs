aa5715cba5ddfdd066390f341f6556c08fe425ad
DESKTOP-RK3QBF1\Bao <quocbaovo06@gmail.com>
2024-06-22 00:31:57 +0700

    init project

---
+++ Plants And Zombies/Assets/Scripts/Services/DependencyInjection/EnvironmentSystem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Services.DependencyInjection
+{
+    public interface IEnvironmentSystem
+    {
+        IEnvironmentSystem ProvideEnvironmentSystem();
+    }
+    
+    public class EnvironmentSystem : MonoBehaviour, IEnvironmentSystem, IDependencyProvider
+    {
+        [Provide]
+        public IEnvironmentSystem ProvideEnvironmentSystem()
+        {
+            return this;
+        }
+    }
+}
